e9f79f81ea2a7a91d27ed04de9ede86aa2d0fb2a
melinab <melinabruhammer2601@gmail.com>
2019-10-24 11:46:29 +0200

    Correction test PageCheckerTest

--- src/test/java/pdl/wiki/PageCheckerTest.java
+++ src/test/java/pdl/wiki/PageCheckerTest.java
@@ -24,11 +24,11 @@ public class PageCheckerTest
     public void existingPagesTest_withHttps()
     {
         Map<String, Integer> urlMap = new HashMap<>();
-        urlMap.put("https://fr.wikipedia.org/wiki/Jeux_olympiques_d'été", 2);
-        urlMap.put("https://fr.wikipedia.org/wiki/Wikipédia:Accueil_principal", 0);
-        urlMap.put("https://fr.wikipedia.org/wiki/Jeux_olympiques", 0);
-        urlMap.put("https://fr.wikipedia.org/wiki/Jeux_olympiques_d'hiver", 4);
-        urlMap.put("https://fr.wikipedia.org/wiki/Jean_Capdouze", 1);
+        urlMap.put("https://fr.wikipedia.org/w/index.php?title=Jeux_olympiques_d%27%C3%A9t%C3%A9&oldid=163777314", 1);
+        urlMap.put("https://fr.wikipedia.org/w/index.php?title=Wikip%C3%A9dia:Accueil_principal&oldid=155532403", 0);
+        urlMap.put("https://fr.wikipedia.org/w/index.php?title=Jeux_olympiques&oldid=163506830", 0);
+        urlMap.put("https://fr.wikipedia.org/w/index.php?title=Jeux_olympiques_d%27hiver&oldid=161852389", 3);
+        urlMap.put("https://fr.wikipedia.org/w/index.php?title=Jean_Capdouze&oldid=162701968", 1);
         for (Map.Entry set : urlMap.entrySet())
         {
             assertEquals((int) set.getValue(), PageChecker.urlCheck(set.getKey().toString()).size());
@@ -43,16 +43,16 @@ public class PageCheckerTest
     public void existingPagesTest_WithoutHttps()
     {
         Map<String, Integer> urlMap = new HashMap<>();
-        urlMap.put("http://fr.wikipedia.org/wiki/Jeux_olympiques_d'été", 2);
-        urlMap.put("fr.wikipedia.org/wiki/Jeux_olympiques_d'été", 2);
-        urlMap.put("http://fr.wikipedia.org/wiki/Wikipédia:Accueil_principal", 0);
-        urlMap.put("fr.wikipedia.org/wiki/Wikipédia:Accueil_principal", 0);
-        urlMap.put("http://fr.wikipedia.org/wiki/Jeux_olympiques", 0);
-        urlMap.put("fr.wikipedia.org/wiki/Jeux_olympiques", 0);
-        urlMap.put("http://fr.wikipedia.org/wiki/Jeux_olympiques_d'hiver", 4);
-        urlMap.put("fr.wikipedia.org/wiki/Jeux_olympiques_d'hiver", 4);
-        urlMap.put("http://fr.wikipedia.org/wiki/Jean_Capdouze", 1);
-        urlMap.put("fr.wikipedia.org/wiki/Jean_Capdouze", 1);
+        urlMap.put("http://fr.wikipedia.org/w/index.php?title=Jeux_olympiques_d%27%C3%A9t%C3%A9&oldid=163777314", 1);
+        urlMap.put("fr.wikipedia.org/w/index.php?title=Jeux_olympiques_d%27%C3%A9t%C3%A9&oldid=163777314", 1);
+        urlMap.put("http://fr.wikipedia.org/w/index.php?title=Wikip%C3%A9dia:Accueil_principal&oldid=155532403", 0);
+        urlMap.put("fr.wikipedia.org/w/index.php?title=Wikip%C3%A9dia:Accueil_principal&oldid=155532403", 0);
+        urlMap.put("http://fr.wikipedia.org/w/index.php?title=Jeux_olympiques&oldid=163506830", 0);
+        urlMap.put("fr.wikipedia.org/w/index.php?title=Jeux_olympiques&oldid=163506830", 0);
+        urlMap.put("http://fr.wikipedia.org/w/index.php?title=Jeux_olympiques_d%27hiver&oldid=161852389", 3);
+        urlMap.put("fr.wikipedia.org/w/index.php?title=Jeux_olympiques_d%27hiver&oldid=161852389", 3);
+        urlMap.put("http://fr.wikipedia.org/w/index.php?title=Jean_Capdouze&oldid=162701968", 1);
+        urlMap.put("fr.wikipedia.org/w/index.php?title=Jean_Capdouze&oldid=162701968", 1);
         for (Map.Entry set : urlMap.entrySet())
         {
             assertEquals((int) set.getValue(), PageChecker.urlCheck(set.getKey().toString()).size());
